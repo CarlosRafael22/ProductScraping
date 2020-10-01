9565dab3666cb2bccf6015aeeca76f9567cdd76c
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-10-01 13:34:36 -0300

    test: Create tests for return product info and dict

--- test_furniture_scraping.py
+++ test_furniture_scraping.py
@@ -1,6 +1,7 @@
 
 import pytest
 from scraping import *
+from products import Product, ProductDatabase
 
 class TestFurnitureRetrieval:
     def test_should_retrieve_furniture_info_list(self):
@@ -12,11 +13,18 @@ class TestFurnitureRetrieval:
         assert html_parsed is not None
         assert type(html_parsed) == BeautifulSoup
     
-    def test_should_get_info_dictionary_about_furnitures(self):
+    def test_should_get_info_list_about_products(self):
         parsed_html = ParsedPage.parsed_html
-        dictionary = get_info_dictionary_about_furnitures(parsed_html)
-        import pdb; pdb.set_trace()
-        assert type(dictionary) == list
+        products = get_info_list_about_products(parsed_html)
+        # import pdb; pdb.set_trace()
+        assert type(products) == list
+
+    def test_should_match_get_info_list_about_products_total_with_product_database_total(self):
+        parsed_html = ParsedPage.parsed_html
+        previous_total = ProductDatabase.get_products_total()
+        products = get_info_list_about_products(parsed_html)
+        # import pdb; pdb.set_trace()
+        assert ProductDatabase.get_products_total() == len(products) + previous_total
     
     currency_test_data = [
         ('1.234,78', 1234.78),
@@ -28,5 +36,13 @@ class TestFurnitureRetrieval:
     def test_should_convert_BRL_currency_to_float(self, currency_str, expected_float):
         value = convert_BRL_currency_to_float(currency_str)
         assert value == expected_float
+    
+    def test_should_get_info_dict_for_product(self):
+        parsed_html = ParsedPage.parsed_html
+        grid_items = parsed_html.find_all("div", class_="product-grid-item")
+        item = grid_items[0]
+        # import pdb; pdb.set_trace()
+        info_dict = get_info_dict_for_product(item)
+        assert type(info_dict) == dict
 
 

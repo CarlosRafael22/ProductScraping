dea7c15360da644f0e8f28a933dc1086275eec70
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2020-10-05 14:50:21 -0300

    feat: Add all methods from scraping.py to PageExtractor + create all tests

--- extractor.py
+++ extractor.py
@@ -1,4 +1,7 @@
 from typing import Tuple
+import requests
+from bs4 import BeautifulSoup, element
+from products import Product
 
 
 class PageExtractor:
@@ -14,7 +17,7 @@ class PageExtractor:
         },
         'americanas': {
             'items': ('div', 'product-grid-item'),
-            'price': ('span', 'PriceUI-sc-1q8ynzz-0 dHyYVS TextUI-sc-12tokcy-0 bLZSPZ'),
+            'price': ('span', 'PriceUI'),
             'name': ('h2', 'TitleUI-bwhjk3-15 khKJTM TitleH2-sc-1wh9e1x-1 gYIWNc'),
             'link': ('a', 'Link-bwhjk3-2'),
             'image': ('img', 'nm-product-img')
@@ -25,6 +28,9 @@ class PageExtractor:
         'magazineluiza': 'https://busca.magazineluiza.com.br/busca?q=',
         'americanas': 'https://www.americanas.com.br/busca/'
     }
+
+    # Will store the BeautifulSoup parsed from the search query when calling retrieve_html_parsed_from_query
+    parsed_html = None
 
     def __init__(self, store_id):
         self.store_id = store_id
@@ -40,7 +46,7 @@ class PageExtractor:
             return tag_and_class
         # return self.STORES_PRODUCTS_PATHS[self.store_id][item_to_be_extracted]
 
-    def get_search_url(self, query):
+    def get_search_url(self, query: str) -> str:
         # This way each func is already executed when creating the dict
         # {'magazineluiza': 'cadeira%20escritorio', 'americanas': 'cadeira-escritorio'}
         # func_dict = {
@@ -56,3 +62,80 @@ class PageExtractor:
         parsed_query = func_dict[self.store_id](query)
 
         return self.STORES_BASE_URLS[self.store_id]+parsed_query
+    
+    def retrieve_html_parsed_from_query(self, query: str) -> BeautifulSoup:
+        ''' Retrieve the first page of the query searched on the e-commerce website parsed to BeautifulSoup object
+        and save it to the parsed_html instance variable '''
+        url = self.get_search_url(query)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # Saving it to the parsed_html variable
+        self.parsed_html = soup
+        return soup
+
+    def get_items_list_from_parsed_html(self, parsed_html: BeautifulSoup) -> element.ResultSet:
+        ''' Returns a list with all the items to be extracted from the given parsed html page '''
+        tag, html_class = self.get_tag_and_class_for_info('items')
+        grid_items = parsed_html.find_all(tag, class_=html_class)
+        return grid_items
+
+    def get_info_list_about_products(self, parsed_html: BeautifulSoup) -> list:
+        ''' Returns a list with all the information about the products from the given parsed html page '''
+        grid_items = self.get_items_list_from_parsed_html(parsed_html)
+        items_list = []
+        print(len(grid_items))
+
+        for item in grid_items:
+            info_dict = self.get_info_dict_for_product(item)
+            items_list.append(info_dict)
+        return items_list
+    
+    def get_info_dict_for_product(self, item) -> dict:
+        ''' Return a dictionary with main information about the product item passed '''
+        import re
+        # image = product_anchor.find('img')
+        img_tag, img_class = self.get_tag_and_class_for_info('image')
+        image = item.find(img_tag, class_=img_class)
+
+        name_tag, name_class = self.get_tag_and_class_for_info('name')
+        name_h2 = item.find(name_tag, class_=name_class)
+
+        link_tag, link_class = self.get_tag_and_class_for_info('link')
+        link = item.find(link_tag, class_=link_class)
+
+        price_tag, price_class = self.get_tag_and_class_for_info('price')
+        regex = re.compile('.*'+price_class+'.*')
+        price_str = item.find(price_tag, { 'class': regex })
+        # name_h2 = item.find("h2", class_="TitleUI-bwhjk3-15 khKJTM TitleH2-sc-1wh9e1x-1 gYIWNc")
+        # name_h2 = item.select(name_extractor)
+        # name_h2 = name_h2[0] if len(name_h2) else None
+        # import pdb; pdb.set_trace()
+
+        name = name_h2.get_text() if name_h2 else 'SEM NOME'
+        # price_str = price_span.get_text() if price_span else 'SEM PRECO'
+        link_url = link.get('href') if link else 'SEM LINK'
+        image_url = image.get('src') if image else 'SEM IMAGEM'
+
+        if price_str:
+            # Value is received like this: 'R$ 1.498,00'
+            price_str = price_str.get_text() 
+            price = PageExtractor.convert_BRL_currency_to_float(price_str[3:])
+        else:
+            price_str = 'SEM PRECO'
+            price = None
+
+        info_dict = {
+            'name': name,
+            'price_str': price_str,
+            'price': price,
+            'link': link_url,
+            'image_url': image_url
+        }
+        Product(info_dict)
+        return info_dict
+    
+    @staticmethod
+    def convert_BRL_currency_to_float(currency_value: str) -> float:
+        # Value is received like this: '1.498,00'
+        float_value = currency_value.replace('.', '/').replace(',', '.').replace('/', '')
+        return float(float_value)
--- test_extractor.py
+++ test_extractor.py
@@ -1,5 +1,8 @@
 import pytest
+from bs4 import BeautifulSoup, element
 from extractor import PageExtractor
+from products import ProductDatabase
+
 
 class TestPageExtractor:
     def test_should_init(self):
@@ -24,3 +27,55 @@ class TestPageExtractor:
     def test_should_get_search_url(self, store, query, expected_url):
         extractor = PageExtractor(store)
         assert extractor.get_search_url(query) == expected_url
+
+    @pytest.mark.parametrize("store,query",[
+        ('magazineluiza', 'Cadeira escritorio'),
+        ('americanas', 'Cadeira escritorio'),
+    ])
+    def test_should_retrieve_html_parsed_from_query(self, store, query):
+        extractor = PageExtractor(store)
+        parsed_html = extractor.retrieve_html_parsed_from_query(query)
+        assert type(parsed_html) == BeautifulSoup
+        assert extractor.parsed_html == parsed_html
+
+    @pytest.mark.parametrize("store,query",[
+        ('magazineluiza', 'Cadeira escritorio'),
+        ('americanas', 'Cadeira escritorio'),
+    ])
+    def test_should_get_items_list_from_parsed_html(self, store, query):
+        extractor = PageExtractor(store)
+        parsed_html = extractor.retrieve_html_parsed_from_query(query)
+        parsed_list = extractor.get_items_list_from_parsed_html(parsed_html)
+        assert type(parsed_list) == element.ResultSet
+        assert type(parsed_list[0]) == element.Tag
+
+    @pytest.mark.parametrize("store,query",[
+        ('magazineluiza', 'Cadeira escritorio'),
+        ('americanas', 'Cadeira escritorio'),
+    ])
+    def test_should_get_info_dict_for_product(self, store, query):
+        extractor = PageExtractor(store)
+        parsed_html = extractor.retrieve_html_parsed_from_query(query)
+        parsed_list = extractor.get_items_list_from_parsed_html(parsed_html)
+        item = extractor.get_info_dict_for_product(parsed_list[0])
+        # import pdb; pdb.set_trace()
+        assert type(parsed_list) == element.ResultSet
+        assert type(item) == dict
+        assert item['name'] is not None
+        assert item['link'] is not None
+
+    @pytest.mark.parametrize("store,query",[
+        # ('magazineluiza', 'Cadeira escritorio'),
+        ('americanas', 'Cadeira escritorio'),
+    ])
+    def test_should_get_info_list_about_products(self, store, query):
+        previous_product_total = ProductDatabase.get_products_total()
+
+        extractor = PageExtractor(store)
+        parsed_html = extractor.retrieve_html_parsed_from_query(query)
+        items_list = extractor.get_info_list_about_products(parsed_html)
+
+        # import pdb; pdb.set_trace()
+        assert type(items_list) == list
+        assert len(items_list) == ProductDatabase.get_products_total() - previous_product_total
+        # assert type(parsed_list[0]) == BeautifulSoup
